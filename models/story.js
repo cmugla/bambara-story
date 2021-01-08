d46fca0ccd3168e932561f26ec4f65d1bb78a526
Celeste Glavin <cglavin@frame.io>
2021-01-07 23:50:49 -0500

    add clickable link

--- models/story.js
+++ models/story.js
@@ -1,39 +1,38 @@
 "use strict";
 
-const { MongoClient } = require('mongodb');
-const dbConnection = process.env['MONGODB_URI'];
+const { MongoClient } = require("mongodb");
+// const dbConnection = process.env['MONGODB_URI'];
 // const dbConnection = 'mongodb://localhost:27017/bambara';
+const dbConnection =
+  "mongodb://celeste:bambara123@cluster-tfm93rzf-shard-00-00.iw7xg.mongodb.net:27017,cluster-tfm93rzf-shard-00-01.iw7xg.mongodb.net:27017,cluster-tfm93rzf-shard-00-02.iw7xg.mongodb.net:27017/heroku_tfm93rzf?ssl=true&replicaSet=atlas-10ap8l-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 function updateIsCovered(req, res, next) {
-  MongoClient.connect(dbConnection, function(err, db) {
-    if(err) throw err;
+  MongoClient.connect(dbConnection, function (err, db) {
+    if (err) throw err;
 
-    db.collection('story')
-      .update(
-        { code: req.params.code },
-        { $set: { isCovered: false } },
-        function(err, results){
-          if(err) throw err;
-          next()
-        }
-      )
-    }
-  )
+    db.collection("story").update(
+      { code: req.params.code },
+      { $set: { isCovered: false } },
+      function (err, results) {
+        if (err) throw err;
+        next();
+      }
+    );
+  });
 }
 
 function getStory(req, res, next) {
-  MongoClient.connect(dbConnection, function(err, db) {
-    if(err) throw err;
+  MongoClient.connect(dbConnection, function (err, db) {
+    if (err) throw err;
 
-    db.collection('story')
+    db.collection("story")
       .find()
-      .toArray(function(err, results) {
+      .toArray(function (err, results) {
         if (err) throw err;
-        res.results = results
-        next()
-      })
-  })
+        res.results = results;
+        next();
+      });
+  });
 }
 
-
-module.exports = { getStory, updateIsCovered }
+module.exports = { getStory, updateIsCovered };
--- src/client/app/Story.js
+++ src/client/app/Story.js
@@ -1,101 +1,107 @@
-import React, { Component } from 'react'
-import Scroll, { Element, scroller } from 'react-scroll'
-import classNames from 'classnames'
+import React, { Component } from "react";
+import Scroll, { Element, scroller } from "react-scroll";
+import classNames from "classnames";
 
-import AjaxAdapter from '../helpers/ajaxAdapter.js'
-import TextInput from './TextInput.js'
+import AjaxAdapter from "../helpers/ajaxAdapter.js";
+import TextInput from "./TextInput.js";
 
-const ajax = new AjaxAdapter(fetch)
+const ajax = new AjaxAdapter(fetch);
 const Config = {
   IS_IOS: !!navigator.userAgent.match(/iPhone|iPad|iPod/i),
-}
+};
 
 class Story extends Component {
   state = {
     story: null,
-    code: '',
+    code: "",
     error: null,
     isReadMode: true,
     isLoading: true,
-  }
+    success: false,
+  };
 
   componentDidMount = () => {
-    ajax.getStory().then(data => {
-      this.setState({ story: data })
-      this.setState({ isLoading: false })
-    })
-  }
+    ajax.getStory().then((data) => {
+      this.setState({ story: data });
+      this.setState({ isLoading: false });
+    });
+  };
 
-  updateCode = e => {
+  updateCode = (e) => {
     var regExOnlyLettersSpaces = /^[a-zA-Z\s]*$/;
     if (!regExOnlyLettersSpaces.test(e.key)) {
-      e.preventDefault()
-      return
+      e.preventDefault();
+      return;
     }
     this.setState({
-      code: e.target.value.split(' ').join('').toLowerCase()
-    })
-  }
+      code: e.target.value.split(" ").join("").toLowerCase(),
+    });
+  };
 
   setIsReadMode = () => {
     this.setState({
       isReadMode: false,
-    })
-  }
+    });
+  };
 
-  handleAnalyticsClickReveal = e => {
-    ga('send', 'event', 'Story', 'click', 'REVEAL');
-  }
+  handleAnalyticsClickReveal = (e) => {
+    ga("send", "event", "Story", "click", "REVEAL");
+  };
 
-  handleClickExternalLink = e => {
-    ga('send', 'event', 'External', 'click', e.target.name);
-  }
+  handleClickExternalLink = (e) => {
+    ga("send", "event", "External", "click", e.target.name);
+  };
 
   handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     // send google anaytics event
-    this.handleAnalyticsClickReveal()
+    this.handleAnalyticsClickReveal();
 
     // validate code --> filter clientside to see if there is a match
-    const successCode = this.state.story.filter((each) => each.code === this.state.code)
+    const successCode = this.state.story.filter(
+      (each) => each.code === this.state.code
+    );
 
     if (successCode.length) {
       // Success!
-      ajax.updateStory(this.state.code).then(data => {
-        // update story
-        this.setState({ story: data })
-        // scroll to element
-        const offset = -((window.innerHeight / 2) - 100)
-        scroller.scrollTo(this.state.code, {
-          duration: 200,
-          delay: 0,
-          smooth: true,
-          offset: offset,
+      ajax
+        .updateStory(this.state.code)
+        .then((data) => {
+          // update story
+          this.setState({ story: data, success: true });
+          // scroll to element
+          const offset = -(window.innerHeight / 2 - 100);
+          scroller.scrollTo(this.state.code, {
+            duration: 200,
+            delay: 0,
+            smooth: true,
+            offset: offset,
+          });
+          // Start download, if not an iOS device (not supported)
+          if (!Config.IS_IOS) {
+            this.download.click();
+            // send event to Google Analytics
+            ga("send", "event", "Story", "click", "Download begins");
+          }
+          this.setState({ isReadMode: true });
         })
-        // Start download, if not an iOS device (not supported)
-        if (!Config.IS_IOS) {
-          this.download.click()
-          // send event to Google Analytics
-          ga('send', 'event', 'Story', 'click', 'Download begins');
-        }
-        this.setState({ isReadMode: true })
-      }).catch(error => console.error('error when updating', error))
+        .catch((error) => console.error("error when updating", error));
     } else {
       // Code not right, show error
       this.setState({
-        error: 'That is not a valid code, please try again.'
-      })
-      ga('send', 'event', 'Invalid Code', 'error');
+        error: "That is not a valid code, please try again.",
+      });
+      ga("send", "event", "Invalid Code", "error");
     }
-  }
+  };
 
-  getStyle = style => {
-    return { fontStyle: style }
-  }
+  getStyle = (style) => {
+    return { fontStyle: style };
+  };
 
   render() {
-    const { story, error, isReadMode, code, isLoading } = this.state
+    const { story, error, isReadMode, code, isLoading } = this.state;
 
     return (
       <div className="story">
@@ -104,19 +110,33 @@ class Story extends Component {
             <p>BAMBARA</p>
             <p>Night Chimes</p>
             <div className={classNames({ hide: !isReadMode })}>
-              <img className="album-art" src="src/client/images/cover.jpg" alt=""/>
-              <button className="button" onClick={this.setIsReadMode}>ENTER DOWNLOAD CODE</button>
+              <img
+                className="album-art"
+                src="src/client/images/cover.jpg"
+                alt=""
+              />
+              <button className="button" onClick={this.setIsReadMode}>
+                ENTER DOWNLOAD CODE
+              </button>
             </div>
-            {
-              !isReadMode
-              &&
-              !Config.IS_IOS
-              &&
+            {!isReadMode && !Config.IS_IOS && (
               <div className="form-container">
-                <p className="instructions">Instructions for digital download:</p>
-                <p className="instructions">Enter the highlighted words on the download card sent to you with your copy of the Night Chimes 7-inch. Click REVEAL.</p>
-                <p className="instructions">Your download will start immediately after your passage is revealed to the world.</p>
-                <form id="code" ref={node => this.form = node} onSubmit={this.handleSubmit}>
+                <p className="instructions">
+                  Instructions for digital download:
+                </p>
+                <p className="instructions">
+                  Enter the highlighted words on the download card sent to you
+                  with your copy of the Night Chimes 7-inch. Click REVEAL.
+                </p>
+                <p className="instructions">
+                  Your download will start immediately after your passage is
+                  revealed to the world.
+                </p>
+                <form
+                  id="code"
+                  ref={(node) => (this.form = node)}
+                  onSubmit={this.handleSubmit}
+                >
                   <TextInput
                     type="text"
                     placeholder="Enter download code"
@@ -124,20 +144,37 @@ class Story extends Component {
                     error={error}
                     onChange={this.updateCode}
                   />
-                  <button className="button" type="submit">REVEAL</button>
-                  <a ref={node => {this.download = node}} href="http://celesteglavin.com/BAMBARA.zip" download="BAMBARA - Night Chimes" />
+                  <button className="button" type="submit">
+                    REVEAL
+                  </button>
+                  <a
+                    ref={(node) => {
+                      this.download = node;
+                    }}
+                    href="http://celesteglavin.com/BAMBARA.zip"
+                    download="BAMBARA - Night Chimes"
+                  />
                 </form>
               </div>
-            }
-            {
-              !isReadMode
-              &&
-              Config.IS_IOS
-              &&
+            )}
+            {!isReadMode && Config.IS_IOS && (
               <div>
-                <p>Darn, iOS devices do not support direct file downloads. Come back to this url on your Mac or PC (or Android device) and follow the instructions for download and, once downloaded, transfer the files to your iOS device.</p>
-                <p>In the meantime, check out Reid&#39;s short story, Night Chimes, below. Enter the highlighted words from the download card to reveal the accompanying unique passage to the world.</p>
-                <form id="code" ref={node => this.form = node} onSubmit={this.handleSubmit}>
+                <p>
+                  Darn, iOS devices do not support direct file downloads. Come
+                  back to this url on your Mac or PC (or Android device) and
+                  follow the instructions for download and, once downloaded,
+                  transfer the files to your iOS device.
+                </p>
+                <p>
+                  In the meantime, check out Reid&#39;s short story, Night
+                  Chimes, below. Enter the highlighted words from the download
+                  card to reveal the accompanying unique passage to the world.
+                </p>
+                <form
+                  id="code"
+                  ref={(node) => (this.form = node)}
+                  onSubmit={this.handleSubmit}
+                >
                   <TextInput
                     type="text"
                     placeholder="Enter download code"
@@ -145,22 +182,53 @@ class Story extends Component {
                     error={error}
                     onChange={this.updateCode}
                   />
-                  <button className="button" type="submit">REVEAL</button>
-                  <a ref={node => {this.download = node}} href="http://celesteglavin.com/BAMBARA.zip" download="BAMBARA - Night Chimes" />
+                  <button className="button" type="submit">
+                    REVEAL
+                  </button>
                 </form>
-                <p className="instructions">Refer to <a name="apple support link" href="https://support.apple.com/en-us/HT205919" onClick={this.handleClickExternalLink}>iTunes Syncing Help</a> for more info on transferring files to your Apple device.</p>
+                <p className="instructions">
+                  Refer to{" "}
+                  <a
+                    name="apple support link"
+                    href="https://support.apple.com/en-us/HT205919"
+                    onClick={this.handleClickExternalLink}
+                  >
+                    iTunes Syncing Help
+                  </a>{" "}
+                  for more info on transferring files to your Apple device.
+                </p>
               </div>
-            }
-            <a name="Night Chimes 7in link" href="https://coldmoonrecords.bandcamp.com/album/night-chimes-7" target="blank" onClick={this.handleClickExternalLink}>Get Night Chimes 7in >>></a>
+            )}
+            {this.state.success && (
+              <a
+                target="new"
+                href="http://celesteglavin.com/BAMBARA.zip"
+                download="BAMBARA - Night Chimes"
+              >
+                if no download triggers, click here
+              </a>
+            )}
+            <br />
+            <br />
+            <a
+              name="Night Chimes 7in link"
+              href="https://coldmoonrecords.bandcamp.com/album/night-chimes-7"
+              target="blank"
+              onClick={this.handleClickExternalLink}
+            >
+              Get Night Chimes 7in {`>>>`}
+            </a>
           </div>
         </div>
         <div className="left-col">
           <div className="story-container">
             <p>Night Chimes</p>
-            <div className={classNames('db-container', { 'show-words': !isLoading })}>
-              {
-                story
-                &&
+            <div
+              className={classNames("db-container", {
+                "show-words": !isLoading,
+              })}
+            >
+              {story &&
                 story.map((each, index) => (
                   <Element
                     key={`line-${index}`}
@@ -168,23 +236,72 @@ class Story extends Component {
                     className="each-line"
                   >
                     <span>
-                      <span style={this.getStyle(each.font_style)} className={`${each.isCovered ? 'is-covered' : ''} ${isReadMode && each.code == code ? 'show' : ''}`}>{each.value} </span>
-                      {each.endOfParagraph && <span><br/><br/></span>} 
+                      <span
+                        style={this.getStyle(each.font_style)}
+                        className={`${each.isCovered ? "is-covered" : ""} ${
+                          isReadMode && each.code == code ? "show" : ""
+                        }`}
+                      >
+                        {each.value}{" "}
+                      </span>
+                      {each.endOfParagraph && (
+                        <span>
+                          <br />
+                          <br />
+                        </span>
+                      )}
                     </span>
                   </Element>
-                ))
-              }
+                ))}
             </div>
             <p>Story by Reid Bateh</p>
-            <p>Music by <a name="BAMBARA bandcamp link" href="https://bambara.bandcamp.com/album/night-chimes" target="blank" onClick={this.handleClickExternalLink}>BAMBARA</a></p>
-            <p><a name="Cold Moon bandcamp link" href="https://coldmoonrecords.bandcamp.com/" target="blank" onClick={this.handleClickExternalLink}>Cold Moon Records</a></p>
-            <p>Website by <a name="Celeste personal link" href="http://celesteglavin.com/" target="blank" onClick={this.handleClickExternalLink}>Celeste Glavin</a></p>
-            <p className="instructions">If you have any questions, please contact Cold Moon Records at <a name="email Cold Moon" href="mailto:coldmoonrecords@gmail.com?subject=Heya%20from%20Bambara%20Night%20Chimes%20website" onClick={this.handleClickExternalLink}>coldmoonrecords@gmail.com</a></p>
+            <p>
+              Music by{" "}
+              <a
+                name="BAMBARA bandcamp link"
+                href="https://bambara.bandcamp.com/album/night-chimes"
+                target="blank"
+                onClick={this.handleClickExternalLink}
+              >
+                BAMBARA
+              </a>
+            </p>
+            <p>
+              <a
+                name="Cold Moon bandcamp link"
+                href="https://coldmoonrecords.bandcamp.com/"
+                target="blank"
+                onClick={this.handleClickExternalLink}
+              >
+                Cold Moon Records
+              </a>
+            </p>
+            <p>
+              Website by{" "}
+              <a
+                name="Celeste personal link"
+                href="http://celesteglavin.com/"
+                target="blank"
+                onClick={this.handleClickExternalLink}
+              >
+                Celeste Glavin
+              </a>
+            </p>
+            <p className="instructions">
+              If you have any questions, please contact Cold Moon Records at{" "}
+              <a
+                name="email Cold Moon"
+                href="mailto:coldmoonrecords@gmail.com?subject=Heya%20from%20Bambara%20Night%20Chimes%20website"
+                onClick={this.handleClickExternalLink}
+              >
+                coldmoonrecords@gmail.com
+              </a>
+            </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Story
+export default Story;
