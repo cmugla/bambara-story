9d70667bb996eb4d72b7993730b7e8bb913e0263
Celeste Glavin <cglavin@frame.io>
2021-01-08 00:22:46 -0500

    nothing to see here

--- models/story.js
+++ models/story.js
@@ -1,10 +1,8 @@
 "use strict";
 
 const { MongoClient } = require("mongodb");
-// const dbConnection = process.env['MONGODB_URI'];
+const dbConnection = process.env["MONGODB_URI"];
 // const dbConnection = 'mongodb://localhost:27017/bambara';
-const dbConnection =
-  "mongodb://celeste:bambara123@cluster-tfm93rzf-shard-00-00.iw7xg.mongodb.net:27017,cluster-tfm93rzf-shard-00-01.iw7xg.mongodb.net:27017,cluster-tfm93rzf-shard-00-02.iw7xg.mongodb.net:27017/heroku_tfm93rzf?ssl=true&replicaSet=atlas-10ap8l-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 function updateIsCovered(req, res, next) {
   MongoClient.connect(dbConnection, function (err, db) {
